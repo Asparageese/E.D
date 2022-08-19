635003106236a237ee1064c01f47b953eba3a385
Asparageese <jeevanpb1337@gmail.com>
2022-08-19 15:17:35 +0100

    0

--- data_pipe.py
+++ data_pipe.py
@@ -4,8 +4,8 @@ from tqdm import tqdm
 from collections import deque
 
 
-input_size = 40
-prediction_entropy = 10
+input_size = 30
+prediction_entropy = 8
 
 data = pd.read_csv("eurusd_minute.csv")
 data = data.drop(columns=['ACh','BCh','Date','Time'])
@@ -13,60 +13,34 @@ data = data.drop(columns=['ACh','BCh','Date','Time'])
 def classify(future,current):
     if future > current:
         return 1
-    if future == current:
-        return 0
     else:
-        return -1
+        return 0
 
-    
-data = data.to_numpy()
-label = deque()
-seq_data = deque()
-for f in range(np.size(data[0,:1])):
-    feature = np.array(data[:,f])
-    seq_data.append(feature)
-    for i in tqdm(range(np.size(data[:,0]))):
-        if i + input_size + prediction_entropy == np.size(data[:,0]):
+#handle single feature associations
+def series_formation(array):
+    series = []
+    for i in tqdm(range(np.size(array))):
+        if (i+input_size+prediction_entropy) == np.size(array):
             break
-        future = data[i+input_size+prediction_entropy,f]
-        current = data[i,f]
-        score = classify(future,current)
-        
-        
-        label.append(score)
+        series.append(array[i:i+input_size])
+    return np.array(series)
 
-del data
-seq_data = np.array(seq_data)        
-#### indexing routing 
+def label_formation(array):
+    labels = []
+    for i in tqdm(range(np.size(array))):
+        if (i+input_size+prediction_entropy) == np.size(array):
+            break
+        labels.append(classify(array[i+input_size+prediction_entropy],array[i+input_size]))
+    return np.array(labels)
 
-index_matrix = []
-for i in tqdm(range(np.size(seq_data[0,:]))):	
-    if i + input_size  == np.size(seq_data[0,:]):
-        break
-    index = np.arange(i,i+input_size)
-    index_matrix.append(index)
+data = data['BO'].to_numpy()
 
+f1 = series_formation(data)
+labels = label_formation(data)
 
-
-label = np.array(label)
-x_data = deque()
-y_data = deque()
-for i in range(np.size(index_matrix)):
-    if i + input_size == np.size(index_matrix):
-        break
-    seriesA = np.take(seq_data,indices=index_matrix[i])
-    x_data.append(seriesA)
-del seq_data
-
-for i in range(np.size(index_matrix)):
-    if i + input_size == np.size(index_matrix):
-        break
-    seriesA = np.take(label,indices=index_matrix[i])
-    y_data.append(seriesA)
+np.save('f1.npy',f1)
+np.save('labels.npy',labels)
 
 
-x_data = np.array(x_data)
-y_data = np.array(y_data)
 
 
-print(np.shape(x_data),np.shape(y_data))
